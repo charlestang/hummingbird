6984c79107b4c0ad38685e8a723f1cccceea905c
Charles <charlestang@foxmail.com>
2016-12-15 01:32:05 +0800

    delete the unused contact form

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -97,24 +97,6 @@ class SiteController extends Controller
     }
 
     /**
-     * Displays contact page.
-     *
-     * @return string
-     */
-    public function actionContact()
-    {
-        $model = new ContactForm();
-        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
-            Yii::$app->session->setFlash('contactFormSubmitted');
-
-            return $this->refresh();
-        }
-        return $this->render('contact', [
-                'model' => $model,
-        ]);
-    }
-
-    /**
      * Displays about page.
      *
      * @return string

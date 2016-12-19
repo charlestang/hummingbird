c9fc223c82284aeea20a169933151f14fcbf97b6
Charles <charlestang@foxmail.com>
2016-12-19 15:17:02 +0800

    optimize the database index html structure

--- views/database/index.php
+++ views/database/index.php
@@ -7,30 +7,47 @@ use yii\grid\GridView;
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = '数据库连接管理';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="database-index">
+<div class="row">
+    <div class="col-xs-12">
+        <div class="box">
+            <div class="box-header">
+                <h3 class="box-title"></h3>
+                <?= Html::a('Create Database', ['create'], ['class' => 'btn btn-success']) ?>
+                <div class="box-tools">
+                    <div class="input-group input-group-sm" style="width: 150px;">
+                        <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
 
-    <p>
-        <?= Html::a('Create Database', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+                        <div class="input-group-btn">
+                            <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body table-responsive no-padding">
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+<?= GridView::widget([
+    'dataProvider' => $dataProvider,
+    'columns' => [
+        ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
-            'alias',
-            'host',
-            'database',
-            'username',
-            // 'password',
-            'charset',
-            'created_at',
-            'updated_at',
+        'id',
+        'alias',
+        'host',
+        'database',
+        'username',
+        // 'password',
+        'charset',
+        'created_at',
+        'updated_at',
 
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
+        ['class' => 'yii\grid\ActionColumn'],
+    ],
+]); ?>
+            </div>
+            <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+    </div>
 </div>

ab1f71f05720ac34ca1449c26348b70bfe4fd06a
Charles <charlestang@foxmail.com>
2016-12-20 01:16:39 +0800

    homepage add link to report create panel

--- views/site/index.php
+++ views/site/index.php
@@ -2,16 +2,16 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = false;
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
         <h1>Congratulations!</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">欢迎使用 Hummingbird 微报表系统</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="<?= yii\helpers\Url::toRoute(['/report/create'])?>">创建报表</a></p>
     </div>
 
     <div class="body-content">

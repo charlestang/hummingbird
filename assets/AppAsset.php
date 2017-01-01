3d2013177c10823ac25151b638790acc36bdc619
Charles <charlestang@foxmail.com>
2017-01-02 00:13:30 +0800

    update the AppAsset comment section

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -1,17 +1,11 @@
 <?php
-/**
- * @link http://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
- */
 
 namespace app\assets;
 
 use yii\web\AssetBundle;
 
 /**
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
+ * @author Charles Tang <charlestang@foxmail.com>
  */
 class AppAsset extends AssetBundle
 {

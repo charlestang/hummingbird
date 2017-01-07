1bdcab267c86568f5798ff2f73c145fd8a25adbb
Charles <charlestang@foxmail.com>
2017-01-08 01:12:15 +0800

    remove unused sidebar menu item

--- views/layouts/left.php
+++ views/layouts/left.php
@@ -47,21 +47,6 @@ JS;
         $this->registerJs($listjs);
         ?>
 
-        <?=
-        \YII_DEBUG ?
-        Menu::widget(
-            [
-                'options' => ['class' => 'sidebar-menu'],
-                'items'   => [
-                    ['label' => 'Menu Yii2', 'options' => ['class' => 'header']],
-                    ['label' => 'Gii', 'icon' => 'fa fa-file-code-o', 'url' => ['/gii']],
-                    ['label' => 'Debug', 'icon' => 'fa fa-dashboard', 'url' => ['/debug']],
-                    ['label' => 'Login', 'url' => ['site/login'], 'visible' => \Yii::$app->user->isGuest],
-                ],
-            ]
-        ):''
-        ?>
-
     </section>
 
 </aside>
